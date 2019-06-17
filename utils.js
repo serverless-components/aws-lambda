feeb066a6bc319d879a43849fbe6162ac3d43f65
Eslam A. Hefnawy <eahefnawy@gmail.com>
2019-06-17 14:33:18 +0300

    added packDir

--- utils.js
+++ utils.js
@@ -1,8 +1,62 @@
 const { tmpdir } = require('os')
 const path = require('path')
-const { readFile } = require('fs-extra')
-const { equals, not, pick } = require('ramda')
+const archiver = require('archiver')
+const globby = require('globby')
+const { contains, isNil, last, split, equals, not, pick } = require('ramda')
+const { readFile, createReadStream, createWriteStream } = require('fs-extra')
 const { utils } = require('@serverless/core')
+
+const VALID_FORMATS = ['zip', 'tar']
+const isValidFormat = (format) => contains(format, VALID_FORMATS)
+
+const packDir = async (inputDirPath, outputFilePath, include = [], exclude = [], prefix) => {
+  const format = last(split('.', outputFilePath))
+
+  if (!isValidFormat(format)) {
+    throw new Error('Please provide a valid format. Either a "zip" or a "tar"')
+  }
+
+  const patterns = ['**']
+
+  if (!isNil(exclude)) {
+    exclude.forEach((excludedItem) => patterns.push(`!${excludedItem}`))
+  }
+
+  const files = (await globby(patterns, { cwd: inputDirPath }))
+    .sort() // we must sort to ensure correct hash
+    .map((file) => ({
+      input: path.join(inputDirPath, file),
+      output: prefix ? path.join(prefix, file) : file
+    }))
+
+  return new Promise((resolve, reject) => {
+    const output = createWriteStream(outputFilePath)
+    const archive = archiver(format, {
+      zlib: { level: 9 }
+    })
+
+    output.on('open', () => {
+      archive.pipe(output)
+
+      // we must set the date to ensure correct hash
+      files.forEach((file) =>
+        archive.append(createReadStream(file.input), { name: file.output, date: new Date(0) })
+      )
+
+      if (!isNil(include)) {
+        include.forEach((file) => {
+          const stream = createReadStream(file)
+          archive.append(stream, { name: path.basename(file), date: new Date(0) })
+        })
+      }
+
+      archive.finalize()
+    })
+
+    archive.on('error', (err) => reject(err))
+    output.on('close', () => resolve(outputFilePath))
+  })
+}
 
 const getAccountId = async (aws) => {
   const STS = new aws.STS()
@@ -189,7 +243,7 @@ const pack = async (code, shims = [], packDeps = true) => {
       .substring(6)}.zip`
   )
 
-  return utils.packDir(code, outputFilePath, shims, exclude)
+  return packDir(code, outputFilePath, shims, exclude)
 }
 
 module.exports = {
