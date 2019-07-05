c3a5521e0607a1f90cbf0bc694edc3d61cd2a171
Eslam A. Hefnawy <eahefnawy@gmail.com>
2019-07-05 18:53:20 +0300

    using resourceId() and other updates

--- serverless.js
+++ serverless.js
@@ -26,11 +26,11 @@ const outputsList = [
   'env',
   'role',
   'layer',
-  'arn'
+  'arn',
+  'region'
 ]
 
 const defaults = {
-  name: 'serverless',
   description: 'AWS Lambda Component',
   memory: 512,
   timeout: 10,
@@ -38,16 +38,22 @@ const defaults = {
   bucket: undefined,
   shims: [],
   handler: 'handler.hello',
-  runtime: 'nodejs8.10',
+  runtime: 'nodejs10.x',
   env: {},
   region: 'us-east-1'
 }
 
 class AwsLambda extends Component {
   async default(inputs = {}) {
+    this.context.status(`Deploying`)
+
     const config = mergeDeepRight(defaults, inputs)
 
-    this.context.status(`Deploying`)
+    config.name = this.state.name || this.context.resourceId()
+
+    this.context.debug(
+      `Starting deployment of lambda ${config.name} to the ${config.region} region.`
+    )
 
     const lambda = new AwsSdkLambda({
       region: config.region,
@@ -59,12 +65,14 @@ class AwsLambda extends Component {
     // If no role exists, create a default role
     let outputsAwsIamRole
     if (!config.role) {
+      this.context.debug(`No role provided for lambda ${config.name}.`)
+
       outputsAwsIamRole = await awsIamRole({
-        name: config.name,
         service: 'lambda.amazonaws.com',
         policy: {
           arn: 'arn:aws:iam::aws:policy/AdministratorAccess'
-        }
+        },
+        region: config.region
       })
       config.role = { arn: outputsAwsIamRole.arn }
     } else {
@@ -74,28 +82,34 @@ class AwsLambda extends Component {
 
     if (
       config.bucket &&
-      config.runtime === 'nodejs8.10' &&
+      config.runtime === 'nodejs10.x' &&
       (await utils.dirExists(path.join(config.code, 'node_modules')))
     ) {
+      this.context.debug(`Bucket ${config.bucket} is provided for lambda ${config.name}.`)
+
       const layer = await this.load('@serverless/aws-lambda-layer')
 
       const layerInputs = {
-        name: `${config.name}-dependencies`,
         description: `${config.name} Dependencies Layer`,
         code: path.join(config.code, 'node_modules'),
-        runtimes: ['nodejs8.10'],
+        runtimes: ['nodejs10.x'],
         prefix: 'nodejs/node_modules',
         bucket: config.bucket,
         region: config.region
       }
 
       this.context.status('Deploying Dependencies')
+      this.context.debug(`Packaging lambda code from ${config.code}.`)
+      this.context.debug(`Uploading dependencies as a layer for lambda ${config.name}.`)
+
       const promises = [pack(config.code, config.shims, false), layer(layerInputs)]
       const res = await Promise.all(promises)
       config.zipPath = res[0]
       config.layer = res[1]
     } else {
       this.context.status('Packaging')
+      this.context.debug(`Packaging lambda code from ${config.code}.`)
+
       config.zipPath = await pack(config.code, config.shims)
     }
 
@@ -110,71 +124,82 @@ class AwsLambda extends Component {
 
     if (!prevLambda) {
       if (config.bucket) {
+        this.context.debug(`Uploading ${config.name} lambda package to bucket ${config.bucket}.`)
         this.context.status(`Uploading`)
+
         await deploymentBucket.upload({ name: config.bucket, file: config.zipPath })
       }
 
       this.context.status(`Creating`)
+      this.context.debug(`Creating lambda ${config.name} in the ${config.region} region.`)
+
       config.arn = await createLambda({ lambda, ...config })
     } else {
       config.arn = prevLambda.arn
       if (configChanged(prevLambda, config)) {
         if (config.bucket && prevLambda.hash !== config.hash) {
           this.context.status(`Uploading`)
+          this.context.debug(`Uploading ${config.name} lambda package to bucket ${config.bucket}.`)
+
           await deploymentBucket.upload({ name: config.bucket, file: config.zipPath })
           await updateLambdaCode({ lambda, ...config })
         }
         this.context.status(`Updating`)
+        this.context.debug(`Updating ${config.name} lambda config.`)
+
         await updateLambdaConfig({ lambda, ...config })
       }
     }
 
+    // todo we probably don't need this logic now thatt we auto generate names
     if (this.state.name && this.state.name !== config.name) {
       this.context.status(`Replacing`)
       await deleteLambda({ lambda, name: this.state.name })
     }
 
-    this.state.name = config.name
-    this.state.arn = config.arn
-    await this.save()
+    this.context.debug(
+      `Successfully deployed lambda ${config.name} in the ${config.region} region.`
+    )
 
     const outputs = pick(outputsList, config)
 
-    this.context.log()
-    this.context.output('name', `       ${outputs.name}`)
-    this.context.output('description', `${outputs.description}`)
-    this.context.output('memory', `     ${outputs.memory}`)
-    this.context.output('timeout', `    ${outputs.timeout}`)
-    this.context.output('arn', `        ${outputs.arn}`)
+    this.state = outputs
+    await this.save()
 
     return outputs
   }
 
-  async remove(inputs = {}) {
-    const config = mergeDeepRight(defaults, inputs)
-    config.name = inputs.name || this.state.name || defaults.name
+  async remove() {
+    this.context.status(`Removing`)
+
+    if (!this.state.name) {
+      this.context.debug(`Aborting removal. Function name not found in state.`)
+      return
+    }
+
+    const { name, region } = this.state
 
     const lambda = new AwsSdkLambda({
-      region: config.region,
+      region,
       credentials: this.context.credentials.aws
     })
-
-    this.context.status(`Removing`)
 
     const awsIamRole = await this.load('@serverless/aws-iam-role')
     const layer = await this.load('@serverless/aws-lambda-layer')
 
-    // there's no need to pass names as input
-    // since it's saved in the child component state
     await awsIamRole.remove()
     await layer.remove()
 
-    await deleteLambda({ lambda, name: config.name })
+    this.context.debug(`Removing lambda ${name} from the ${region} region.`)
+    await deleteLambda({ lambda, name })
+    this.context.debug(`Successfully removed lambda ${name} from the ${region} region.`)
+
+    const outputs = pick(outputsList, this.state)
 
     this.state = {}
     await this.save()
 
-    return {}
+    return outputs
   }
 }
 
