78ca533d71620e4e88673ed60db0292fd17627fe
ac360 <austencollins@gmail.com>
2020-01-28 04:22:35 -0800

    refactor cloud version

--- src/serverless.js
+++ src/serverless.js
@@ -1,128 +1,45 @@
 const { Component } = require('@serverless/core')
-const {
-  getConfig,
-  getClients,
-  createRole,
-  removeRole,
-  createLambda,
-  updateLambdaCode,
-  updateLambdaConfig,
-  getLambda,
-  deleteLambda,
-  configChanged,
-  pack,
-  hashFile
-} = require('./utils')
+const { publishLayer, createLayerBucket } = require('./utils')
 
-class AwsLambda extends Component {
+class AwsLambdaLayer extends Component {
   async deploy(inputs = {}) {
-    await this.status(`Deploying`)
 
-    const config = getConfig(inputs, this)
+    inputs.region = inputs.region || 'us-east-1'
+    inputs.name = inputs.name || 'layer-component'
+    inputs.runtimes = inputs.runtimes || []
 
-    await this.debug(`Starting deployment of lambda ${config.name} to the ${config.region} region.`)
-
-    const { lambda, iam } = getClients(this.credentials.aws, config.region)
-
-    // If no role exists, create a default role
-    if (!config.roleArn) {
-      await this.debug(
-        `No role provided for lambda ${config.name}.  Creating/Updating default IAM Role with basic execution rights...`
-      )
-      config.roleArn = await createRole(iam, config)
-      this.state.roleArn = config.roleArn
-      await this.save()
+    // Detect bucket
+    if (!this.state.bucketName) {
+      console.log('Creating a layer bucket')
+      const bucket = await createLayerBucket(this.credentials.aws, inputs.region)
+      console.log('Created: ', bucket)
+      this.state.bucketName = bucket
     }
 
-    await this.status('Packaging')
-    await this.debug(`Packaging lambda code from ${config.src}.`)
-    config.zipPath = await pack(config.src, config.shims)
+    this.state.region = inputs.region || this.state.region || 'us-east-1'
+    this.state.name = inputs.name || this.state.name || 'layer-component-' + Math.random().toString(36).substring(7)
 
-    config.hash = await hashFile(config.zipPath)
-
-    const prevLambda = await getLambda({ lambda, ...config })
-
-    if (!prevLambda) {
-      await this.status(`Creating`)
-      await this.debug(`Creating lambda ${config.name} in the ${config.region} region.`)
-
-      const createResult = await createLambda(lambda, config)
-      config.arn = createResult.arn
-      config.hash = createResult.hash
-    } else {
-      config.arn = prevLambda.arn
-      if (configChanged(prevLambda, config)) {
-        if (prevLambda.hash !== config.hash) {
-          await this.status(`Uploading code`)
-          await this.debug(`Uploading ${config.name} lambda code.`)
-          await updateLambdaCode(lambda, config)
-        }
-
-        await this.status(`Updating`)
-        await this.debug(`Updating ${config.name} lambda config.`)
-        const updateResult = await updateLambdaConfig(lambda, config)
-        config.hash = updateResult.hash
-      }
-    }
-
-    // todo we probably don't need this logic now that we auto generate names
-    if (this.state.name && this.state.name !== config.name) {
-      await this.status(`Replacing`)
-      await deleteLambda({ lambda, name: this.state.name })
-    }
-
-    await this.debug(`Successfully deployed lambda ${config.name} in the ${config.region} region.`)
-
-    this.state = config
-    await this.save()
+    const res = await publishLayer(this.credentials.aws, inputs.region, inputs.src, this.state.name, this.state.bucketName, inputs.runtimes)
+    
+    this.state.arn = res.LayerArn
+    this.state.arnVersion = res.LayerVersionArn
 
     return {
-      name: config.name,
-      arn: config.arn,
-      securityGroupIds: config.securityGroupIds,
-      subnetIds: config.subnetIds
+      name: this.state.name,
+      region: this.state.region,
+      arn: this.state.arn,
+      arnVersion: this.state.arnVersion,
+      bucketName: this.state.bucketName,
     }
-  }
-
-  async publishVersion() {
-    const { name, region, hash } = this.state
-
-    const { lambda } = getClients(this.credentials.aws, region)
-
-    const { Version } = await lambda
-      .publishVersion({
-        FunctionName: name,
-        CodeSha256: hash
-      })
-      .promise()
-
-    return { version: Version }
   }
 
   async remove() {
-    await this.status(`Removing`)
+    return {}
+  }
 
-    if (!this.state.name) {
-      await this.debug(`No state found.  Function appears removed already.  Aborting.`)
-      return
-    }
-
-    const { name, region } = this.state
-
-    const { iam, lambda } = getClients(this.credentials.aws, region)
-
-    if (this.state.roleArn) {
-      await this.debug(`Removing role with arn ${this.state.roleArn}.`)
-      await removeRole(iam, this.state)
-    }
-
-    await this.debug(`Removing lambda ${name} from the ${region} region.`)
-    await deleteLambda({ lambda, name })
-    await this.debug(`Successfully removed lambda ${name} from the ${region} region.`)
-
-    this.state = {}
-    await this.save()
+  extract() {
+    return false
   }
 }
 
-module.exports = AwsLambda
+module.exports = AwsLambdaLayer
--- src/utils.js
+++ src/utils.js
@@ -1,409 +1,56 @@
 const AWS = require('aws-sdk')
-const { tmpdir } = require('os')
 const path = require('path')
-const crypto = require('crypto')
-const archiver = require('archiver')
-const globby = require('globby')
-const { contains, isNil, last, split, equals, not, pick, endsWith } = require('ramda')
-const { readFile, createReadStream, createWriteStream } = require('fs-extra')
-const fs = require('fs')
+const shortid = require('shortid')
+const { readFileSync, readdirSync, removeSync } = require('fs-extra')
 
 const sleep = async (wait) => new Promise((resolve) => setTimeout(() => resolve(), wait))
 
-const VALID_FORMATS = ['zip', 'tar']
-const isValidFormat = (format) => contains(format, VALID_FORMATS)
-
-const packDir = async (inputDirPath, outputFilePath, include = [], exclude = [], prefix) => {
-  const format = last(split('.', outputFilePath))
-
-  if (!isValidFormat(format)) {
-    throw new Error('Please provide a valid format. Either a "zip" or a "tar"')
+/**
+ * Create Layer Bucket
+ */
+const createLayerBucket = async(credentials, region) => {
+  const s3 = new AWS.S3({ credentials, region })
+  const Bucket = 'layer-code-' + Math.random().toString(36).substring(7) // TODO: Not long enough.  Collision risk.
+  const params = {
+    Bucket,
+    ACL: 'private',
   }
-
-  const patterns = ['**']
-
-  if (!isNil(exclude)) {
-    exclude.forEach((excludedItem) => patterns.push(`!${excludedItem}`))
-  }
-
-  const files = (await globby(patterns, { cwd: inputDirPath, dot: true }))
-    .sort() // we must sort to ensure correct hash
-    .map((file) => ({
-      input: path.join(inputDirPath, file),
-      output: prefix ? path.join(prefix, file) : file
-    }))
-
-  return new Promise((resolve, reject) => {
-    const output = createWriteStream(outputFilePath)
-    const archive = archiver(format, {
-      zlib: { level: 9 }
-    })
-
-    output.on('open', () => {
-      archive.pipe(output)
-
-      // we must set the date to ensure correct hash
-      files.forEach((file) =>
-        archive.append(createReadStream(file.input), { name: file.output, date: new Date(0) })
-      )
-
-      if (!isNil(include)) {
-        include.forEach((file) => {
-          const stream = createReadStream(file)
-          archive.append(stream, { name: path.basename(file), date: new Date(0) })
-        })
-      }
-
-      archive.finalize()
-    })
-
-    archive.on('error', (err) => reject(err))
-    output.on('close', () => resolve(outputFilePath))
-  })
+  await s3.createBucket(params).promise()
+  return Bucket
 }
 
-const getAccountId = async (aws) => {
-  const STS = new aws.STS()
-  const res = await STS.getCallerIdentity({}).promise()
-  return res.Account
-}
-
-const randomId = Math.random()
-  .toString(36)
-  .substring(6)
-
-const hashFile = async (filePath) =>
-  crypto
-    .createHash('sha256')
-    .update(await readFile(filePath))
-    .digest('base64')
-
-const getClients = (credentials, region) => {
-  const iam = new AWS.IAM({ credentials, region })
+const publishLayer = async (credentials, region, zipPath, layerName, bucketName, runtimes = []) => {
+  const s3 = new AWS.S3({ credentials, region })
   const lambda = new AWS.Lambda({ credentials, region })
 
-  return {
-    iam,
-    lambda
+  const keyName = `layer-${Date.now()}`
+
+  var params = { 
+    Bucket: bucketName, 
+    Key: keyName, 
+    Body: readFileSync(zipPath)
   }
-}
+  var options = { partSize: 10 * 1024 * 1024, queueSize: 1 }
+  const upload = await s3.upload(params, options).promise()
 
-const writeDevModeConfigFile = (config, instance) => {
-  const devModeConfig = {
-    platformStage: process.env.SERVERLESS_PLATFORM_STAGE,
-    accessKey: instance.accessKey,
-    instanceId: `${instance.org}.${instance.app}.${instance.stage}.${instance.name}`,
-    userHandler: config.handler
-  }
-
-  const devModeConfigFilePath = path.join(config.src, `_devMode.json`)
-
-  fs.writeFileSync(devModeConfigFilePath, JSON.stringify(devModeConfig), 'utf8')
-}
-
-const getConfig = (inputs, instance) => {
-  const config = {
-    name:
-      inputs.name || instance.state.name || `aws-lambda-component-${instance.stage}-${randomId}`,
-    roleArn: inputs.roleArn || instance.state.roleArn,
-    roleName: `lambda-role-${instance.stage}-${randomId}`,
-    description: inputs.description || 'AWS Lambda Component',
-    memory: inputs.memory || 512,
-    timeout: inputs.timeout || 10,
-    src: inputs.src || process.cwd(),
-    shims: inputs.shims || [],
-    handler: inputs.handler || 'index.handler',
-    runtime: 'nodejs12.x',
-    env: inputs.env || {},
-    region: inputs.region || 'us-east-1',
-    securityGroupIds: inputs.vpcConfig ? inputs.vpcConfig.securityGroupIds : false,
-    subnetIds: inputs.vpcConfig ? inputs.vpcConfig.subnetIds : false
-  }
-
-  // setup dev mode
-  // config.env.SERVERLESS_PLATFORM_STAGE = process.env.SERVERLESS_PLATFORM_STAGE
-  // config.env.SERVERLESS_ACCESS_KEY = instance.accessKey
-  // config.env.SERVERLESS_COMPONENT_INSTANCE_ID = `${instance.org}.${instance.app}.${instance.stage}.${instance.name}`
-  // config.env.USER_HANDLER = config.handler
-
-  writeDevModeConfigFile(config, instance)
-
-  config.handler = '_handler.handler'
-
-  return config
-}
-
-const createRole = async (iam, config) => {
-  const assumeRolePolicyDocument = {
-    Version: '2012-10-17',
-    Statement: {
-      Effect: 'Allow',
-      Principal: {
-        Service: ['lambda.amazonaws.com']
-      },
-      Action: 'sts:AssumeRole'
-    }
-  }
-  const res = await iam
-    .createRole({
-      RoleName: config.roleName,
-      Path: '/',
-      AssumeRolePolicyDocument: JSON.stringify(assumeRolePolicyDocument)
-    })
-    .promise()
-
-  await iam
-    .attachRolePolicy({
-      RoleName: config.roleName,
-      PolicyArn: 'arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole'
-    })
-    .promise()
-
-  return res.Role.Arn
-}
-
-const removeRole = async (iam, config) => {
-  if (!config.roleArn) {
-    return
-  }
-  try {
-    await iam
-      .detachRolePolicy({
-        RoleName: config.roleArn.split('/')[1], // extract role name from arn
-        PolicyArn: 'arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole'
-      })
-      .promise()
-    await iam
-      .deleteRole({
-        RoleName: config.roleArn.split('/')[1]
-      })
-      .promise()
-  } catch (error) {
-    if (error.code !== 'NoSuchEntity') {
-      throw error
-    }
-  }
-}
-
-const createLambda = async (lambda, config) => {
-  const params = {
-    FunctionName: config.name,
-    Code: {},
-    Description: config.description,
-    Handler: config.handler,
-    MemorySize: config.memory,
-    Publish: true,
-    Role: config.roleArn,
-    Runtime: config.runtime,
-    Timeout: config.timeout,
-    Environment: {
-      Variables: config.env
+  const layerParams = {
+    Content: {
+      S3Bucket: bucketName,
+      S3Key: upload.Key,
     },
-    ...(config.securityGroupIds && {
-      VpcConfig: {
-        SecurityGroupIds: config.securityGroupIds,
-        SubnetIds: config.subnetIds
-      }
-    })
+    LayerName: layerName, /* required */
+    CompatibleRuntimes: runtimes
   }
+  const res = await lambda.publishLayerVersion(layerParams).promise()
 
-  if (config.layer && config.layer.arn) {
-    params.Layers = [config.layer.arn]
-  }
+  // Clean up the zip
+  removeSync(zipPath)
 
-  params.Code.ZipFile = await readFile(config.zipPath)
-
-  try {
-    const res = await lambda.createFunction(params).promise()
-    return { arn: res.FunctionArn, hash: res.CodeSha256 }
-  } catch (e) {
-    if (e.message.includes(`The role defined for the function cannot be assumed by Lambda`)) {
-      // we need to wait around 9 seconds after the role is craated before it can be assumed
-      await sleep(1000)
-      return createLambda(lambda, config)
-    }
-    throw e
-  }
-}
-
-const updateLambdaConfig = async (lambda, config) => {
-  const functionConfigParams = {
-    FunctionName: config.name,
-    Description: config.description,
-    Handler: config.handler,
-    MemorySize: config.memory,
-    Role: config.roleArn,
-    Runtime: config.runtime,
-    Timeout: config.timeout,
-    Environment: {
-      Variables: config.env
-    },
-    ...(config.securityGroupIds
-      ? {
-          VpcConfig: {
-            SecurityGroupIds: config.securityGroupIds,
-            SubnetIds: config.subnetIds
-          }
-        }
-      : {
-          VpcConfig: {
-            SecurityGroupIds: [],
-            SubnetIds: []
-          }
-        })
-  }
-
-  if (config.layer && config.layer.arn) {
-    functionConfigParams.Layers = [config.layer.arn]
-  }
-
-  const res = await lambda.updateFunctionConfiguration(functionConfigParams).promise()
-
-  return { arn: res.FunctionArn, hash: res.CodeSha256 }
-}
-
-const updateLambdaCode = async (lambda, config) => {
-  const functionCodeParams = {
-    FunctionName: config.name,
-    Publish: true
-  }
-
-  functionCodeParams.ZipFile = await readFile(config.zipPath)
-  const res = await lambda.updateFunctionCode(functionCodeParams).promise()
-
-  return res.FunctionArn
-}
-
-const getLambda = async ({ lambda, name }) => {
-  try {
-    const res = await lambda
-      .getFunctionConfiguration({
-        FunctionName: name
-      })
-      .promise()
-
-    return {
-      name: res.FunctionName,
-      description: res.Description,
-      timeout: res.Timeout,
-      runtime: res.Runtime,
-      role: {
-        arn: res.Role
-      },
-      handler: res.Handler,
-      memory: res.MemorySize,
-      hash: res.CodeSha256,
-      env: res.Environment ? res.Environment.Variables : {},
-      arn: res.FunctionArn,
-      securityGroupIds: res.VpcConfig ? res.VpcConfig.SecurityGroupIds : false,
-      subnetIds: res.VpcConfig ? res.VpcConfig.SubnetIds : false
-    }
-  } catch (e) {
-    if (e.code === 'ResourceNotFoundException') {
-      return null
-    }
-    throw e
-  }
-}
-
-const deleteLambda = async ({ lambda, name }) => {
-  try {
-    const params = { FunctionName: name }
-    await lambda.deleteFunction(params).promise()
-  } catch (error) {
-    if (error.code !== 'ResourceNotFoundException') {
-      throw error
-    }
-  }
-}
-
-const getPolicy = async ({ name, region, accountId }) => {
-  return {
-    Version: '2012-10-17',
-    Statement: [
-      {
-        Action: ['logs:CreateLogStream'],
-        Resource: [`arn:aws:logs:${region}:${accountId}:log-group:/aws/lambda/${name}:*`],
-        Effect: 'Allow'
-      },
-      {
-        Action: ['logs:PutLogEvents'],
-        Resource: [`arn:aws:logs:${region}:${accountId}:log-group:/aws/lambda/${name}:*:*`],
-        Effect: 'Allow'
-      }
-    ]
-  }
-}
-
-const configChanged = (prevLambda, lambda) => {
-  const keys = [
-    'description',
-    'runtime',
-    'roleArn',
-    'handler',
-    'memory',
-    'timeout',
-    'env',
-    'hash',
-    'securityGroupIds',
-    'subnetIds'
-  ]
-  const inputs = pick(keys, lambda)
-  const prevInputs = pick(keys, prevLambda)
-  return not(equals(inputs, prevInputs))
-}
-
-const pack = async (code, shims = [], packDeps = true) => {
-  if (endsWith('.zip', code) || endsWith('.tar', code)) {
-    return path.resolve(code)
-  }
-
-  let exclude = []
-
-  if (!packDeps) {
-    exclude = ['node_modules/**']
-  }
-
-  const outputFilePath = path.join(
-    tmpdir(),
-    `${Math.random()
-      .toString(36)
-      .substring(6)}.zip`
-  )
-
-  const includeDirectory = path.join(__dirname, 'include')
-  const devModeIncludes = [
-    // dev-mode
-    path.join(includeDirectory, 'centra.js'),
-    path.join(includeDirectory, 'CentraRequest.js'),
-    path.join(includeDirectory, 'CentraResponse.js'),
-    path.join(includeDirectory, 'find-port.js'),
-    path.join(includeDirectory, 'get-port.js'),
-    path.join(includeDirectory, 'json-buffer.js'),
-    path.join(includeDirectory, 'phin.js'),
-    path.join(includeDirectory, 'sdk.js'),
-    path.join(includeDirectory, 'streamLog.js'),
-    path.join(includeDirectory, 'sync-rpc.js'),
-    path.join(includeDirectory, 'worker.js'),
-    path.join(includeDirectory, '_handler.js')
-  ]
-
-  return packDir(code, outputFilePath, shims.concat(devModeIncludes), exclude)
+  return res
 }
 
 module.exports = {
-  getConfig,
-  getClients,
-  createRole,
-  removeRole,
-  createLambda,
-  updateLambdaCode,
-  updateLambdaConfig,
-  getLambda,
-  deleteLambda,
-  getPolicy,
-  getAccountId,
-  configChanged,
-  pack,
-  hashFile
+  createLayerBucket,
+  publishLayer,
+  sleep,
 }
